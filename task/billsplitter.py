0a979c14357717b92672125c23921a4e9b42929b
Junior Brito <junior.br.pr@gmail.com>
2024-06-18 18:52:40 +0200

    Passes stage 2

--- task/billsplitter.py
+++ task/billsplitter.py
@@ -4,4 +4,7 @@ if number_of_friends <= 0:
 else:
     print("Enter the name of every friend (including you), each on a new line:")
     friends = {input(): 0 for _ in range(number_of_friends)}
+    bill = int(input("Enter the total bill value:\n"))
+    for friend in friends:
+        friends[friend] = round(bill / number_of_friends, 2)
     print(friends)
